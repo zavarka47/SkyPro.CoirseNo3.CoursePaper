4762da4deecdfefe3002f8cb7d282866b1456678
Zavarka47 <pazuzu1995@gmail.com>
2023-03-29 21:38:39 +0300

    add @operation

--- src/main/java/com/example/coursepaperno3/service/Impl/SocksServiceImpl.java
+++ src/main/java/com/example/coursepaperno3/service/Impl/SocksServiceImpl.java
@@ -5,31 +5,36 @@ import com.example.coursepaperno3.model.CottonPart;
 import com.example.coursepaperno3.model.Size;
 import com.example.coursepaperno3.model.Sock;
 import com.example.coursepaperno3.service.SocksService;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
+@Service
 public class SocksServiceImpl implements SocksService {
 
-    private final Map <Sock, Integer> socks = new TreeMap<>();
-    private final Map <LocalDate, HashMap<Sock, Integer>> deleteHistory = new TreeMap<>();
-
-
+    //private final Map<Sock, Integer> socks = new TreeMap<>();
+    //private final List <Sock> socks = new ArrayList<>();
+    private final Map <Sock, Integer> socks = new HashMap<>();
+    private final Map<LocalDate, HashMap<Sock, Integer>> deleteHistory = new TreeMap<>();
 
     //Post
-    public void addSocks (Color color, Size size, CottonPart cottonPart, int quantity){
+    @Override
+    public boolean addSocks(Color color, Size size, CottonPart cottonPart, int quantity) {
         Sock sockNew = new Sock(color, size, cottonPart);
-        if (socks.containsKey(sockNew)){
+        if (quantity<0){return false;}
+        if (socks.containsKey(sockNew)) {
             socks.put(sockNew, (socks.get(sockNew) + quantity));
+            return true;
         } else {
             socks.put(sockNew, quantity);
+            return true;
         }
     }
 
     //Put
-    public boolean putSocks (Color color, Size size, CottonPart cottonPart, int quantity) {
+    @Override
+    public boolean putSocks(Color color, Size size, CottonPart cottonPart, int quantity) {
         Sock sockPut = new Sock(color, size, cottonPart);
         if (!socks.containsKey(sockPut) || (socks.get(sockPut) < quantity)) {
             return false;
@@ -40,21 +45,30 @@ public class SocksServiceImpl implements SocksService {
     }
 
     //Get
-    public Integer getSocks (Color color, Size size, CottonPart cottonPart){
+    @Override
+    public Integer getSocks(Color color, Size size, CottonPart cottonPart) {
         Sock sockGet = new Sock(color, size, cottonPart);
         return socks.getOrDefault(sockGet, -1);
     }
+
     //Delete
-    public boolean deleteSocks (Color color, Size size, CottonPart cottonPart, int quantity){
+    @Override
+    public boolean deleteSocks(Color color, Size size, CottonPart cottonPart, int quantity) {
         Sock sockDelete = new Sock(color, size, cottonPart);
         if (!socks.containsKey(sockDelete) || (socks.get(sockDelete) < quantity)) {
             return false;
         } else {
             socks.put(sockDelete, (socks.get(sockDelete) - quantity));
-            HashMap <Sock, Integer> test = new HashMap<>();
-            test.put(sockDelete, quantity);
-            deleteHistory.put(LocalDate.now(), test);
+            if (deleteHistory.containsKey(LocalDate.now())) {
+                deleteHistory.get(LocalDate.now()).put(sockDelete, quantity);
+            } else {
+                HashMap<Sock, Integer> socksDelete = new HashMap<>();
+                socksDelete.put(sockDelete, quantity);
+                deleteHistory.put(LocalDate.now(), socksDelete);
+            }
             return true;
         }
+
+
     }
 }
--- src/main/java/com/example/coursepaperno3/service/SocksService.java
+++ src/main/java/com/example/coursepaperno3/service/SocksService.java
@@ -1,4 +1,19 @@
 package com.example.coursepaperno3.service;
 
+import com.example.coursepaperno3.model.Color;
+import com.example.coursepaperno3.model.CottonPart;
+import com.example.coursepaperno3.model.Size;
+
 public interface SocksService {
+    //Post
+    boolean addSocks(Color color, Size size, CottonPart cottonPart, int quantity);
+
+    //Put
+    boolean putSocks(Color color, Size size, CottonPart cottonPart, int quantity);
+
+    //Get
+    Integer getSocks(Color color, Size size, CottonPart cottonPart);
+
+    //Delete
+    boolean deleteSocks(Color color, Size size, CottonPart cottonPart, int quantity);
 }
